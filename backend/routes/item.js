c6cbbb1cd0f1d21320b93c4527a981d6d5e7bd06
Martyna <martynalecka.ml@gmail.com>
2019-12-08 23:31:27 +0100

    Added router for getting all items and item by id.

---
+++ backend/routes/item.js
@@ -0,0 +1,16 @@
+const {Item, validateItem} = require('../models/item');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const items = await Item.find().sort('slot');
+    res.send(items);
+});
+
+router.get('/:id', async (req, res) => {
+    const item = await Item.findById(req.params.id);
+    if(!item) return res.status(404).send('The item with the given ID was not found.');
+    res.send(item);
+});
+
+module.exports = router;
