1f807e2e0e79d2cc444ee0ff50adb549e4dc8f4d
Martyna <martynalecka.ml@gmail.com>
2019-12-11 18:43:27 +0100

    Included changes in models, fix problem with inventory/backpack router

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,7 +1,10 @@
-const {Inventory, validateInventory} = require('../models/inventory');
-const {Item, validateItem} = require('../models/item');
+const {inventory, validateInventory} = require('../models/inventory');
+const {item, validateItem} = require('../models/item');
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Inventory = mongoose.model('inventories', inventory);
+const Item = mongoose.model('items', item);
 
 router.get('/:id', async (req, res) => {
     const inventory = await Inventory.findById(req.params.id);
@@ -10,29 +13,27 @@ router.get('/:id', async (req, res) => {
 });
 
 router.put('/:id/gold', async (req, res) => {
-    const { error } = validateInventory(req.body.gold);
+    const { error } = validateInventory(req.body.inventory);
     if(error) return res.status(400).send(error.details[0].message);
 
     const inventory = await Inventory.findByIdAndUpdate(req.params.id,
         {
-            gold: req.body.gold
+            gold: req.body.inventory.gold
         }, { new: true });
     if(!inventory) return res.status(404).send('The inventory with the given ID was not found.');
     res.send(inventory);
 });
 
 router.put('/:id/backpack', async (req, res) => {
-    const { error } = validateInventory(req.body.backpack);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    const item = await Item.findById(req.body.id);
+    const item = await Item.findById(req.body._id);
     if(!item) return res.status(400).send('Invalid item.');
+
+    const inventoryHandel = await Inventory.findById(req.params.id, 'backpack', { lean: true });
+    inventoryHandel.backpack.push(req.body._id);
 
     const inventory = await Inventory.findByIdAndUpdate(req.params.id,
         {
-            backpack: {
-                _id: item._id
-            }
+            backpack: inventoryHandel.backpack
         }, { new: true });
 
     if(!inventory) return res.status(404).send('The inventory with the given ID was not found.');
